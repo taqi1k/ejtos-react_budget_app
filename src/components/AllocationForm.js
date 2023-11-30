1f6fcdfcee97ad3476fb2f6d8d5f5cd5e907c2ff
Taqi Syed <t.syed64@outlook.com>
2023-11-29 18:34:52 -0600

    Updated

--- src/components/AllocationForm.js
+++ src/components/AllocationForm.js
@@ -2,78 +2,132 @@ import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const AllocationForm = (props) => {
-    const { dispatch,remaining  } = useContext(AppContext);
+    const { dispatch, remaining } = useContext(AppContext);
 
     const [name, setName] = useState('');
     const [cost, setCost] = useState('');
     const [action, setAction] = useState('');
+    const [selectedCurrency, setSelectedCurrency] = useState('£'); // Default currency Pound
+
+    const currencies = ['$ Dollars', '£ Euros', '€ Pounds', '₹ Rupees']; // Available currencies
+
+    const isFormValid = cost > 0 && cost <= remaining;
 
     const submitEvent = () => {
-
-            if(cost > remaining) {
-                alert("The value cannot exceed remaining funds  £"+remaining);
-                setCost("");
-                return;
-            }
+        if (!isFormValid) {
+            alert(`Please enter a valid cost between 1 and £${remaining}`);
+            return;
+        }
 
         const expense = {
             name: name,
             cost: parseInt(cost),
+            currency: selectedCurrency, // Include the selected currency in the expense object
         };
-        if(action === "Reduce") {
+
+        if (action === 'Reduce') {
             dispatch({
                 type: 'RED_EXPENSE',
                 payload: expense,
             });
         } else {
-                dispatch({
-                    type: 'ADD_EXPENSE',
-                    payload: expense,
-                });
-            }
+            dispatch({
+                type: 'ADD_EXPENSE',
+                payload: expense,
+            });
+        }
+
+        // Clear form fields after submission
+        setName('');
+        setCost('');
+        setAction('');
+        setSelectedCurrency('£'); // Reset currency to default after submission
     };
 
     return (
         <div>
             <div className='row'>
-
-            <div className="input-group mb-3" style={{ marginLeft: '2rem' }}>
+                <div className="input-group mb-3" style={{ marginLeft: '2rem' }}>
                     <div className="input-group-prepend">
-                <label className="input-group-text" htmlFor="inputGroupSelect01">Department</label>
-                  </div>
-                  <select className="custom-select" id="inputGroupSelect01" onChange={(event) => setName(event.target.value)}>
+                        <label className="input-group-text" htmlFor="inputGroupSelect01">
+                            Department
+                        </label>
+                    </div>
+                    <select
+                        className="custom-select"
+                        id="inputGroupSelect01"
+                        onChange={(event) => setName(event.target.value)}
+                        value={name}
+                    >
                         <option defaultValue>Choose...</option>
-                        <option value="Marketing" name="marketing"> Marketing</option>
-                <option value="Sales" name="sales">Sales</option>
-                <option value="Finance" name="finance">Finance</option>
-                <option value="HR" name="hr">HR</option>
-                <option value="IT" name="it">IT</option>
-                <option value="Admin" name="admin">Admin</option>
-                  </select>
+                        <option value="Marketing">Marketing</option>
+                        <option value="Sales">Sales</option>
+                        <option value="Finance">Finance</option>
+                        <option value="HR">HR</option>
+                        <option value="IT">IT</option>
+                        <option value="Admin">Admin</option>
+                    </select>
 
                     <div className="input-group-prepend" style={{ marginLeft: '2rem' }}>
-                <label className="input-group-text" htmlFor="inputGroupSelect02">Allocation</label>
-                  </div>
-                  <select className="custom-select" id="inputGroupSelect02" onChange={(event) => setAction(event.target.value)}>
-                        <option defaultValue value="Add" name="Add">Add</option>
-                <option value="Reduce" name="Reduce">Reduce</option>
-                  </select>
+                        <label className="input-group-text" htmlFor="inputGroupSelect02">
+                            Allocation
+                        </label>
+                    </div>
+                    <select
+                        className="custom-select"
+                        id="inputGroupSelect02"
+                        onChange={(event) => setAction(event.target.value)}
+                        value={action}
+                    >
+                        <option value="Add">Add</option>
+                        <option value="Reduce">Reduce</option>
+                    </select>
 
+                    <div className="input-group-prepend" style={{ marginLeft: '2rem' }}>
+                        <span className="input-group-text" id="changeAllocationText">
+                            {selectedCurrency}
+                        </span>
+                    </div>
+                    <div>
                     <input
                         required='required'
                         type='number'
                         id='cost'
                         value={cost}
-                        style={{ marginLeft: '2rem' , size: 10}}
-                        onChange={(event) => setCost(event.target.value)}>
-                        </input>
+                        style={{ marginLeft: '2rem' }}
+                        onChange={(event) => setCost(event.target.value)}
+                    />
+                    </div>
 
-                    <button className="btn btn-primary" onClick={submitEvent} style={{ marginLeft: '2rem' }}>
+                    {/* New currency dropdown */}
+                    <div className="input-group-prepend" style={{ marginLeft: '2rem' }}>
+                        <label className="input-group-text" htmlFor="inputGroupSelectCurrency">
+                            Currency
+                        </label>
+                    </div>
+                    <select
+                        className="custom-select"
+                        id="inputGroupSelectCurrency"
+                        onChange={(event) => setSelectedCurrency(event.target.value)}
+                        value={selectedCurrency}
+                    >
+                        {currencies.map((currency) => (
+                            <option key={currency} value={currency}>
+                                {currency}
+                            </option>
+                        ))}
+                    </select>
+
+                    <button
+                        className="btn btn-primary"
+                        onClick={submitEvent}
+                        style={{ marginLeft: '2rem' }}
+                        disabled={!isFormValid}
+                    >
                         Save
                     </button>
                 </div>
-                </div>
-
+            </div>
         </div>
     );
 };
--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,18 +1,22 @@
-
 import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const { budget, selectedCurrency } = useContext(AppContext);
     const [newBudget, setNewBudget] = useState(budget);
+
     const handleBudgetChange = (event) => {
         setNewBudget(event.target.value);
-    }
+    };
+
     return (
-<div className='alert alert-secondary'>
-<span>Budget: £{budget}</span>
-<input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
-</div>
+        <div className='alert alert-secondary'>
+            <span>
+                Budget: {selectedCurrency}
+            </span>
+            <input type="number" step="10" value={newBudget} onChange={handleBudgetChange} />
+        </div>
     );
 };
+
 export default Budget;
